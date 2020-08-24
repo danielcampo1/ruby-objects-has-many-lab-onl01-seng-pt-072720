aa6c307ac52c7789ba45e3c0eca8cb824065ee61
daniel campo <campo_daniel94@yahoo.com>
2020-08-24 21:32:03 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+    attr_accessor :posts, :name
+    @@all = []
+        def initialize(name)
+            @name = name
+            @posts = []
+        end
+
+    def add_post(post)
+        @posts << post
+        post.author = self
+    end
+
+    def add_post_by_title(title)
+        post = Author.new(title)
+        add_post(title)
+    end
+    
+    def post
+    Post.all.select do |post|
+      post.author == self
+      end
+   end
+
+
+    def self.post_count
+        Author.all.count
+    end
+
+end
